14ce140a4c8a082facce6381d9e6cfd42cc49b09
Ameera <ameera.gahussain@gmail.com>
2025-01-06 20:38:05 +0800

    remove mdx code

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    { enforce: 'pre', ...mdx() },
-    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
-    react({ jsxImportSource: '@emotion/react' })
-    react({
-      babel: {
-        presets: [...],
-        // Your plugins run before any built-in transform (eg: Fast Refresh)
-        plugins: [...],
-        // Use .babelrc files
-        babelrc: true,
-        // Use babel.config.js files
-        configFile: true,
-      }})
-  ],
+  plugins: [react()],
+  base: "/ameera-gahussain.github.io/",
 });
