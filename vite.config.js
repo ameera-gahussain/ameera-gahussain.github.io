6cf735b7603c11584a1c184a1ae49aa6591d2ebf
Ameera <ameera.gahussain@gmail.com>
2025-01-06 20:27:53 +0800

    add jsx runtime

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,16 @@ export default defineConfig({
   plugins: [
     { enforce: 'pre', ...mdx() },
     react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+    react({ jsxImportSource: '@emotion/react' })
+    react({
+      babel: {
+        presets: [...],
+        // Your plugins run before any built-in transform (eg: Fast Refresh)
+        plugins: [...],
+        // Use .babelrc files
+        babelrc: true,
+        // Use babel.config.js files
+        configFile: true,
+      }})
   ],
 });
